741d4114ff483bc6e0c2eb69d775b8464adc1cb0
JuanManuelEsquivel <manuel.esquivels@academicos.udg.mx>
2020-07-24 17:23:43 -0500

    Archivo python con una clase que hace el contacto

---
+++ contacto.py
@@ -0,0 +1,24 @@
+class Contacto:
+
+    def __init__(self, info):
+        if isinstance(info, str):
+            self.nombre = info
+            self.direccion = ""
+            self.numero_de_medidor = ""
+            self.medida_anterior = ""
+            self.medida_actual = ""
+            self.periodo = ""
+            self.fecha_ultimo_abono = ""
+        elif isinstance(info, dict):
+            self.nombre = info['nombre']
+            self.direccion = info['direccion']
+            self.numero_de_medidor = info['numero_de_medidor']
+            self.medida_anterior = info['medida_anterior']
+            self.medida_actual = info['medida_actual']
+            self.periodo = info['periodo']
+            self.fecha_ultimo_abono = info['fecha_ultimo_abono']
+        else:
+            raise TypeError
+
+    def obtenerDatos(self):
+        return self.__dict__
